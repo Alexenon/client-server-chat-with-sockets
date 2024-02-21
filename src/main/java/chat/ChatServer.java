65871aba2c23c8bc02821583b4dfecfc4f16efa0
Alex Xenon <b3haply@gmail.com>
2024-02-21 20:03:43 +0200

    Started implementing private messages

--- src/main/java/chat/ChatServer.java
+++ src/main/java/chat/ChatServer.java
@@ -8,6 +8,13 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+// TODO:
+//  - chat layout
+//  - encryption
+//  - private messages
+//  - commands
 
 public class ChatServer {
     private static final int PORT = 8080;
@@ -33,7 +40,9 @@ public class ChatServer {
 
     static class ClientHandler extends Thread {
         private final Socket socket;
-        private PrintWriter out;
+        private String username;
+        private BufferedReader input;
+        private PrintWriter output;
 
         public ClientHandler(Socket socket) {
             this.socket = socket;
@@ -41,16 +50,16 @@ public class ChatServer {
 
         public void run() {
             try {
-                out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                output = new PrintWriter(socket.getOutputStream(), true);
+                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                String username = in.readLine();
-                System.out.println("User connected: " + username);
+                output.println("Successfully connected to the server");
+                askUsername();
 
                 broadcastMessage("User " + username + " joined the chat");
 
                 String inputLine;
-                while ((inputLine = in.readLine()) != null) {
+                while ((inputLine = input.readLine()) != null) {
                     System.out.println("Received from " + username + ": " + inputLine);
                     broadcastMessage(username + ": " + inputLine);
                 }
@@ -65,9 +74,52 @@ public class ChatServer {
             }
         }
 
+        public void askUsername() throws IOException {
+            output.println("Please enter your username:");
+            username = input.readLine();
+            System.out.println("User connected: " + username);
+        }
+
         private void broadcastMessage(String message) {
+//            if (isPrivateMessage(message)) {
+//                sendPrivateMessage(message);
+//            } else {
+//                sendPublicMessage(message);
+//            }
+
+            System.out.println("broadcasting: " + message);
+
+            sendPublicMessage(message);
+        }
+
+        /**
+         * Format: `username: username: Text`
+         */
+        private void sendPrivateMessage(String message) {
+            String[] args = message.split(" ");
+            String usernameReceiver = args[0]
+                    .replace(":", "")
+                    .toLowerCase();
+
+            Optional<ClientHandler> receiver = clients.stream()
+                    .filter(client -> client.username.toLowerCase().equals(usernameReceiver))
+                    .findFirst();
+
+            String textToBeDisplayed = receiver.isPresent()
+                    ? message
+                    : "User " + usernameReceiver + " is not found";
+
+            receiver.ifPresent(clientHandler -> clientHandler.output.println(textToBeDisplayed));
+            this.output.println(textToBeDisplayed); // Show the message to the sender also
+        }
+
+        private boolean isPrivateMessage(String message) {
+            return message.split(" ")[0].matches("\\w+:");
+        }
+
+        private void sendPublicMessage(String message) {
             for (ClientHandler client : clients) {
-                client.out.println(message);
+                client.output.println(message);
             }
         }
     }
